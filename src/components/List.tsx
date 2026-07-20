a2533d9759ecd866dec0d90c1ac8003f45d7b819
Elise <86999190+elisethewizard@users.noreply.github.com>
2026-07-20 19:50:00 +0300

    cleanup

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,9 @@
-import type { Film } from '../types.ts'
-import Card from './Card.tsx'
+import type { Film } from "../types"
+import Card from "./Card"
 
 function List(props: { data: Film[] }) {
     const { data } = props
-
+    
     return (
         <div className='main-content list-cont'>
             { data.map(element => <Card film={element} key={element.id} />) }
