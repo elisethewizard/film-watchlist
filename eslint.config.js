38c2cabd3f303202c119b8a7466b74497bd1dfa6
Elise <86999190+elisethewizard@users.noreply.github.com>
2026-09-16 16:32:40 +0300

    updated config & deps

--- eslint.config.js
+++ eslint.config.js
@@ -3,34 +3,25 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config(
-    { ignores: ['dist'] },
+export default defineConfig([
+    globalIgnores(['dist']),
     {
+        files: ['**/*.{ts,tsx}'],
         extends: [
             js.configs.recommended,
-            ...tseslint.configs.recommendedTypeChecked,
-            ...tseslint.configs.stylisticTypeChecked,
+            tseslint.configs.strictTypeChecked,
+            tseslint.configs.stylisticTypeChecked,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
         ],
-        files: ['**/*.{ts,tsx}'],
         languageOptions: {
-            ecmaVersion: 2020,
             globals: globals.browser,
             parserOptions: {
                 project: ['./tsconfig.node.json', './tsconfig.app.json'],
                 tsconfigRootDir: import.meta.dirname,
-            }
-        },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
+            },
         },
     },
-)
+])
